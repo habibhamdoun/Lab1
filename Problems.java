e482ace29049ee55148cf8bfa6c04b4bfd74cfd1
habibhamdoun <100615843+habibhamdoun@users.noreply.github.com>
2023-01-26 20:13:54 +0200

    Correcting the code in Problems.java

---
+++ Problems.java
@@ -0,0 +1,8 @@
+public class Problems{
+	public static void main(String [] args){
+		System.out.print("Hi,can we check this? ");
+		System.out.println("it has a lot of problems");
+		System.out.println("Yeah , I know !!!");
+		// This a comment
+	}
+}	
