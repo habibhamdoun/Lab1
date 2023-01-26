1bd18bf9a7e6864534faca5616a79844efbaa639
habibhamdoun <100615843+habibhamdoun@users.noreply.github.com>
2023-01-26 20:21:40 +0200

    initial info provided

---
+++ Profile.java
@@ -0,0 +1,7 @@
+public class Profile{
+	public static void main(String[] args){
+		System.out.println("Habib hamdoun");
+		System.out.println("Computer Engineering");
+		System.out.println("First Year");
+	}
+}
